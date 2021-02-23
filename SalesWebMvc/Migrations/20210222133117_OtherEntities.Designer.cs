8b521b8b33c0ee3dd108b2ca2d3803a340f4798e
Joao Marco <joaomarco.matta@gmail.com>
2021-02-23 12:03:20 -0300

    Fixed BaseSalary attribute name in Sellers. Implementing SellerService and basic FindAll.

--- SalesWebMvc/Migrations/20210222133117_OtherEntities.Designer.cs
+++ SalesWebMvc/Migrations/20210222133117_OtherEntities.Designer.cs
@@ -56,7 +56,7 @@ namespace SalesWebMvc.Migrations
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd();
 
-                    b.Property<double>("BaleSalary");
+                    b.Property<double>("BaseSalary");
 
                     b.Property<DateTime>("BirthDate");
 
